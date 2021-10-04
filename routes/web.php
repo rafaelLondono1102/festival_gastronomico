c7a5af2dfe472320b1741850bdb8be5628c706fd
Rafael Londoño Botero <rafael.londonob@autonoma.edu.co>
2021-10-04 18:03:40 -0500

    Se agrega el modulo del Middleware( auth )

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,16 @@ use App\Http\Controllers\RestaurantController;
 }); */
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-///////////////////////////////////////////////////////////////////////////
+//Publicas
 Route::get('/', [App\Http\Controllers\RestaurantController::class, 'showFrontPage'])->name('front_page.index');
+///////////////////////////////////////////////////////////////////////////
+//Privadas
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resource('restaurants', App\Http\Controllers\RestaurantController::class);
+});
+
 
 /* Route::get('/restaurants', [App\Http\Controllers\RestaurantController::class, 'index'])->name('restaurants.index');
 Route::get('/restaurants/{restaurant}', [App\Http\Controllers\RestaurantController::class, 'show'])->name('restaurants.show');
 Route::get('/restaurants', [App\Http\Controllers\RestaurantController::class, 'create'])->name('restaurants.create'); */
-
-Route::resource('restaurants', App\Http\Controllers\RestaurantController::class);
