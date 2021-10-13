c6e74d45f04ba67b35178e7e533671e6abd9da59
Rafael Londoño Botero <rafael.londonob@autonoma.edu.co>
2021-10-13 08:34:17 -0500

    Se pueden crear los mensajes en los restaurantes

--- resources/views/restaurants/show.blade.php
+++ resources/views/restaurants/show.blade.php
@@ -12,26 +12,44 @@
             Contactenos al numero {{ $restaurant->phone}}<br>
        @endif
         <br>
+
+       <div class="jumbotron">
+           <h3>Cuentanos que opinas !</h3>
+            {!! Form::open(['route' => ['comments.store'],'method' => 'post']) !!}
+                <div class="mb-3">
+                    {{ Form::label('comment','Comentario',['class' => 'form-label']) }}
+                    {{ Form::text('comment',null,['class' => 'form-control']) }}
+                    {{ Form::hidden('restaurant_id', $restaurant->id),[] }}
+                </div>
+                <div class="mb-3">
+                    {{ Form::label('score','Puntuacion',['class' => 'form-label']) }}
+                    {{ Form::select('score', [1 => '1 estrella', 2 => '2 estrellas', 3 => '3 estrellas', 4 => '4 estrellas', 5 => '5 estrellas'],null,['class' => 'form-control']); }}
+                </div>
+                {{ Form::submit('Crear',['class' => 'btn btn-primary']) }}
+            {!! Form::close() !!}
+       </div>
+
+
+
         @foreach ($comments as $comment)
-            <div class="row">
+            <div class="row mt-3">
                 <div class="col">
                     <ol class="list-group list-group-numbered">
                         <li class="list-group-item d-flex justify-content-between align-items-start">
-                        <div class="ms-2">
-                            <div class="fw-bold">
-                                <div class="row">
-                                    <b>{{ $comment->user->name}}</b>
-                                    @for ($i = 0; $i < $comment->score; $i++)
-                                    <i class="fas fa-star"></i>  
-                                    @endfor
+                            <div class="ms-2">
+                                <div class="fw-bold">
+                                    <div class="row">
+                                        <b>{{ $comment->user->name}}</b>
+                                        @for ($i = 0; $i < $comment->score; $i++)
+                                        <i class="fas fa-star"></i>  
+                                        @endfor
+                                    </div>
                                 </div>
+                                {{ $comment->comment }}
                             </div>
-                            {{ $comment->comment }}
-                        </div>
                         </li>
                     </ol>
                 </div>
-                
             </div>
         @endforeach
 
