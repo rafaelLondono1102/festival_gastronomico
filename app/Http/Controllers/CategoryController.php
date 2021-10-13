9ce7262637af935afa1faa26de1f2831d71faf24
Rafael Londoño Botero <rafael.londonob@autonoma.edu.co>
2021-10-12 21:30:06 -0500

    Se añade la funcionalidad del lista cartegorias

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,7 +14,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories=Category::orderBy('name','asc')->get();
+        return view('categories.index',compact('categories'));
     }
 
     /**
